89a0ffef6c84101da9ac4f652311a4c0ae8e8382
毕一然 <1229139809@qq.com>
2023-03-10 09:00:02 +0800

    1

--- server/storage/database.cpp
+++ server/storage/database.cpp
@@ -1,24 +1,57 @@
 #include "database.h"
 #include "../parse/parse_defs.h"
-DataBase::DataBase()
+#include "../storage/storage_defs.h"
+DataBase::DataBase(const char * database_name)
 {
-    database_name_ = nullptr;
+    database_name_ = strnew(database_name);
     database_dfile_ = nullptr;
 }
-bool DataBase::initialize(const char * name)
+RE DataBase::initialize()
 {
-    GlobalParamsManager::getInstance().getBinDir();
+    //open dfile 
+    if(isExists())
+    {
+        char *const bin_path = GlobalParamsManager::getInstance().getBinPath();
+        printf("bin_path:%s\n",bin_path);
+        return RE::SUCCESS;
+    }
+    return RE::FAIL;
 }
 
-void DataBase::create()
+RE DataBase::create()
 {
-
+    if(isExists())
+    {
+        return RE::FAIL;
+    }
+    const char * bin_path=GlobalParamsManager::getInstance().getBinPath();
+    return RE::SUCCESS;
 }
 
+void DataBase::destroy()
+{
+    delete[] database_name_;
+    if(database_dfile_!=nullptr);
+        fclose(database_dfile_);
+}
+
+bool DataBase::isExists()
+{
+    dirent* a;
+    DIR* const bin_dir=GlobalParamsManager::getInstance().getBinDir();
+    if((a=findDir(bin_dir,database_name_))==nullptr)
+        return false;
+    return true;
+}
 DataBaseManager &DataBaseManager::getInstance()
 {
     static DataBaseManager instance;
     return instance;
 }
 
+void DataBaseManager::initialize(){    }
 
+RE DataBaseManager::createDB(const char *database_name)
+{
+    return RE();
+}
--- server/storage/database.h
+++ server/storage/database.h
@@ -5,31 +5,36 @@
 class DataBase
 {
 public:
-    DataBase();
-    bool initialize(const char * name);
-    //if not exists,create it 
-    void create();
-    //delete the database
-    void destruction();
-    //destroy this
+    DataBase(const char *database_name);
+    RE initialize();
+    // if not exists,create it
+    RE create();
+    // delete the database
+    RE destruction();
+    // destroy this
     void destroy();
-private:
-    bool ifExists();
-    char *database_name_;
-    FILE* database_dfile_;
-};
+    char *getDBName() { return database_name_; }
+    FILE *getDBDFile() { return database_dfile_; }
 
+private:
+    bool isExists();
+    char *database_name_;
+    FILE *database_dfile_;
+};
 
 class DataBaseManager
 {
 public:
-    static DataBaseManager& getInstance();
+    static DataBaseManager &getInstance();
     void initialize();
-    void open();
+    RE createDB(const char * database_name);
+    RE openDB(const char * databse_name);
+    RE delDB(const char * database_name);
+    RE closeDB(const char * database_name);
 private:
     DataBaseManager();
     ~DataBaseManager();
-    std::map<const char *,DataBase*> opened_databases;
+    std::map<const char *, DataBase *> opened_databases;
 };
 
 // class GlobalParamsManager
