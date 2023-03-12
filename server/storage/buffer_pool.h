d52efa57e0cc2cd37176ffa477d1f751d7da5ea7
毕一然 <1229139809@qq.com>
2023-03-12 16:20:17 +0800

    1

--- server/storage/buffer_pool.h
+++ server/storage/buffer_pool.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "page.h"
-#define BP_FILE_HDR_SIZE (sizeof(BPFileSubHeader))
-struct BPFileHeader {
-  int32_t page_count;        //! 当前文件一共有多少个页面
-  int32_t allocated_pages;   //! 已经分配了多少个页面
-  char    bitmap[0];         //! 页面分配位图, 第0个页面(就是当前页面)，总是1
+#define BP_FILE_HDR_SIZE (sizeof(BPFileHeader))
+struct BPFileHeader
+{
+    int32_t page_count;      //! 当前文件一共有多少个页面
+    int32_t allocated_pages; //! 已经分配了多少个页面
+    char bitmap[0];          //! 页面分配位图, 第0个页面(就是当前页面)，总是1
 
-  /**
-   * 能够分配的最大的页面个数，即bitmap的字节数 乘以8
-   */
-  static const int MAX_PAGE_NUM = (BP_PAGE_DATA_SIZE - sizeof(page_count) - sizeof(allocated_pages)) * 8;
+    /**
+     * 能够分配的最大的页面个数，即bitmap的字节数 乘以8
+     */
+    // file header是一页 使用该页存储 字节*8个bit
+    static const int MAX_PAGE_NUM = (BP_PAGE_DATA_SIZE - sizeof(page_count) - sizeof(allocated_pages)) * 8;
 };
--- server/storage/page.h
+++ server/storage/page.h
@@ -1,13 +1,10 @@
 #pragma once
 #include <stdint.h>
-#define BP_INVALID_PAGE_NUM (-1) 
+#define BP_INVALID_PAGE_NUM (-1)
 #define BP_PAGE_SIZE (1 << 14)
 #define BP_PAGE_DATA_SIZE (BP_PAGE_SIZE - sizeof(int32_t))
-#define BP_FILE_SUB_HDR_SIZE (sizeof(BPFileSubHeader))
 struct Page
 {
     int32_t page_id;
     char page_data[BP_PAGE_SIZE];
 };
-
-
