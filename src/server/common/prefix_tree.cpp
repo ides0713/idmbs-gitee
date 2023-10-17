2e5efa2c052eeaa84e806900229f130463b67e48
毕一然 <1229139809@qq.com>
2023-10-17 16:42:04 +0800

    INIT

--- src/server/common/prefix_tree.cpp
+++ src/server/common/prefix_tree.cpp
@@ -1,1 +1,130 @@
 #include "prefix_tree.h"
+#include <algorithm>
+#include <cctype>
+PrefixTreeNode::PrefixTreeNode() {
+    height = 0;
+    end = false;
+    type = WordType::Undefined;
+}
+PrefixTreeNode::PrefixTreeNode(const char &c) {
+    value = c;
+    height = 0;
+    end = false;
+    type = WordType::Undefined;
+}
+PrefixTreeNode::~PrefixTreeNode() {
+    for (auto node: childs)
+        delete node;
+}
+int PrefixTreeNode::FindChild(char value) {
+    for (int i = 0; i < childs.size(); i++) {
+        if (value == childs[i]->value)
+            return i;
+    }
+    return -1;
+}
+void PrefixTreeNode::Desc(std::ostream &os) {
+    os << "[node_info] value:" << value << " height:" << height << " childs_num:" << childs.size() << " end:" << end
+       << std::endl;
+}
+PrefixTree::PrefixTree() {
+    root_ = new PrefixTreeNode('\0');
+}
+void PrefixTree::Insert(char *value, int length, WordType word_type) {
+    PrefixTreeNode *ptr = root_;
+    int begin_pos = SkipPrefix(ptr, value, length);
+    for (int i = begin_pos; i < length; i++) {
+        PrefixTreeNode *new_node = new PrefixTreeNode(value[i]);
+        ptr->childs.push_back(new_node);
+        new_node->height = ptr->height + 1;
+        ptr = new_node;
+    }
+    if (ptr->end == false) {
+        ptr->end = true;
+        ptr->type = word_type;
+    }
+}
+void PrefixTree::Insert(std::string value, WordType word_type) {
+    Insert(const_cast<char *>(value.c_str()), value.length(), word_type);
+}
+Suffixes *PrefixTree::GetSuffixes(std::string value) {
+    return GetSuffixes(const_cast<char *>(value.c_str()), value.length());
+}
+void PrefixTree::Delete(char *value, int length) {
+    PrefixTreeNode *ptr = root_, *delete_parent_node = nullptr;
+    int delete_node_index = -1;
+    for (int i = 0; i < length; i++) {
+        if (ptr->childs.empty())
+            return;
+        else {
+            int res = ptr->FindChild(value[i]);
+            if (res == -1)
+                return;
+            if (ptr->childs.size() > 1) {
+                delete_parent_node = ptr;
+                delete_node_index = res;
+            }
+            ptr = ptr->childs[i];
+        }
+    }
+    if (ptr->end == false)
+        return;
+    if (!ptr->childs.empty()) {
+        ptr->end = false;
+    } else {
+        PrefixTreeNode *delete_node = ptr->childs[delete_node_index];
+        ptr->childs.erase(ptr->childs.begin() + delete_node_index);
+        delete delete_node;
+    }
+}
+Suffixes *PrefixTree::GetSuffixes(char *prefix, int length) {
+    PrefixTreeNode *ptr = root_;
+    int skip_chars = SkipPrefix(ptr, prefix, length);
+    if (skip_chars != length)
+        return nullptr;
+    Suffixes ses;
+    if (ptr->childs.empty()) {
+        ses.suffixes.push_back("");
+    } else if (ptr->childs.size() == 1) {
+    } else if (ptr->c)
+        return nullptr;
+}
+void PrefixTree::Delete(std::string value) {
+    Delete(const_cast<char *>(value.c_str()), value.length());
+}
+int PrefixTree::SkipPrefix(PrefixTreeNode *&ptr, char *value, int length) {
+    for (int i = 0; i < length; i++) {
+        // ptr->Desc(std::cout);
+        if (ptr->childs.empty())
+            return ptr->height;
+        else {
+            int res_index = ptr->FindChild(value[i]);
+            if (res_index == -1)
+                return ptr->height;
+            ptr = ptr->childs[res_index];
+        }
+    }
+    return ptr->height;
+}
+int PrefixTree::FindWord(std::string value) {
+    return FindWord(const_cast<char *>(value.c_str()), value.length());
+}
+int PrefixTree::FindWord(char *value, int length) {
+    PrefixTreeNode *ptr = root_;
+    for (int i = 0; i < length; i++) {
+        if (ptr->childs.empty())
+            return 0;
+        else {
+            int res_index = ptr->FindChild(value[i]);
+            if (res_index == -1)
+                return 0;
+            ptr = ptr->childs[res_index];
+        }
+    }
+    return 1;
+}
+void Suffixes::Desc() {
+    int index = 0;
+    for (const auto &str: suffixes_)
+        printf("suffix %d:%s\n", index++, str);
+}
--- src/server/common/prefix_tree.h
+++ src/server/common/prefix_tree.h
@@ -1,5 +1,64 @@
 #pragma once
-class PrefixTree{
-    public:
-    private:
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+enum WordType
+{
+    Undefined = 0,
+    KeyWord,
+    TableName,
+    DatabaseName,
+    AttrName
+};
+struct PrefixTreeNode {
+public:
+    PrefixTreeNode();
+    PrefixTreeNode(const char &c);
+    ~PrefixTreeNode();
+    int FindChild(char value);
+    void Desc(std::ostream &os);
+
+public:
+    char value;
+    std::vector<PrefixTreeNode *> childs;
+    int height;
+    bool end;
+    WordType type;
+};
+struct Suffix {
+public:
+    Suffix();
+    Suffix(WordType type);
+
+public:
+    WordType type;
+    std::string str;
+};
+struct Suffixes {
+public:
+    Suffixes() = default;
+    void Desc();
+
+public:
+    std::vector<std::string> suffixes;
+};
+class PrefixTree
+{
+public:
+    PrefixTree();
+    void Insert(std::string value, WordType word_type);
+    void Delete(std::string value);
+    Suffixes *GetSuffixes(std::string value);
+    int FindWord(std::string value);
+
+private:
+    int SkipPrefix(PrefixTreeNode *&ptr, char *value, int length);
+    void Insert(char *value, int length, WordType word_type);
+    void Delete(char *value, int length);
+    Suffixes *GetSuffixes(char *prefix, int length);
+    int FindWord(char *value, int length);
+
+private:
+    PrefixTreeNode *root_;
 };
